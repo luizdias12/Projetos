23478ec952ac5a6c0f7c0cc0cb0954828366e24e
luizdias12 <lewis.sas@gmail.com>
2022-03-23 11:21:19 -0300

    firebolt

--- Node/firebolt/app.js
+++ Node/firebolt/app.js
@@ -6,8 +6,14 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var aboutRouter = require('./routes/about');
 
 var app = express();
+
+//Bootstrap e Jquery
+app.use('/css',express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js',express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+app.use('/js',express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/about', aboutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
