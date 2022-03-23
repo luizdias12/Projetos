de5225abf3f6d57ed1b1b8b6c1b86d2b742f798b
luizdias12 <lewis.sas@gmail.com>
2022-03-23 13:27:30 -0300

    commit

--- Node/firebolt/app.js
+++ Node/firebolt/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var aboutRouter = require('./routes/about');
+var applicationRouter = require('./routes/application');
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/about', aboutRouter);
+app.use('/application', applicationRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
