8d37324e272c24ed830dfd4ef5c74a1dcf569a9a
luizdias12 <lewis.sas@gmail.com>
2023-06-23 10:37:47 -0300

    commit 23062023

--- Web/js/genchar.js
+++ Web/js/genchar.js
@@ -1,4 +1,11 @@
 const races = ["Humano", "Anão", "Elfo", "Meio-Elfo", "Tiefling", "Halfling", "Draconato", "Meio-Orc", "Gnomo"];
+const racesVal = ["human", "dwarf", "elf", "halfelf", "tiefling", "halfling", "dragonborn", "halforc", "gnome"];
+const genders = ["male", "female"];
+
+const objGender = {
+    "male": "Masculino",
+    "female": "Feminino"
+};
 
 const objRace = {
     "elf": "Elfo",
@@ -45,6 +52,7 @@ class Person {
         let anonymous = new Person(name)
         anonymous.setRace(race)
         anonymous.setClass(genClass())
+        anonymous.setGender(gender)
         return anonymous
     }
     getClass() {
@@ -59,6 +67,48 @@ class Person {
     setRace(race) {
         this.race = objRace[race]
     }
+    getGender() {
+        return this.gender
+    }
+    setGender(gender) {
+        this.gender = objGender[gender]
+    }
+    getStr() {
+        return this.str
+    }
+    setStr(str) {
+        this.str = str
+    }
+    getDex() {
+        return this.dex
+    }
+    setDex(dex) {
+        this.dex = dex
+    }
+    getCon() {
+        return this.con
+    }
+    setCon(con) {
+        this.con = con
+    }
+    getWis() {
+        return this.wis
+    }
+    setWis(wis) {
+        this.wis = wis
+    }
+    getInt() {
+        return this.int
+    }
+    setInt(int) {
+        this.int = int
+    }
+    getChr() {
+        return this.chr
+    }
+    setChr(chr) {
+        this.chr = chr
+    }
 }
 
 function getRdm(obj) {
@@ -67,21 +117,42 @@ function getRdm(obj) {
 }
 
 function genName(race, gender) {
+    // Anao
     let nomeAnaoM = ["Adrik", "Alberich", "Baern", "Bruenor", "Eberk", "Flint", "Harbek", "Morgran", "Rangrim", "Thoradin", "Thorin", "Travok", "Ulfgar"];
     let nomeAnaoF = ["Amber", "Artin", "Bardryn", "Eldeth", "Gurdis", "Helja", "Kathra", "Ilde", "Riswynn", "Torbera", "Torgga", "Vistra"];
     let sobrenomeAnao = ["Balderk", "Battlehammer", "Dankil", "Fireforge", "Frostbeard", "Ironfist", "Leutgehr", "Rummaheim", "Ungart"];
-
+    // Elfo
     let nomeElfoM = ["Adran", "Aramil", "Berrian", "Enialis", "Erevan", "Immeral", "Ivelios", "Laucian", "Paelias", "Peren", "Quarion", "Soveliss", "Varis"];
     let nomeElfoF = ["Adrie", "Antinus", "Birel", "Caelyn", "Enna", "Ielenia", "Leshanna", "Meriele", "Mialee", "Sariel", "Thia", "Vadania", "Valanthe"];
     let sobrenomeElfo = ["Amakir", "Amastacia", "Galanodel", "Holimion", "Liadon", "Nailo", "Siannodel"];
-
+    // Humano
     let nomeHumanoM = ["Darvin", "Dorn", "Evendur", "Gorstag", "Grim", "Helm", "Igan", "Ivor", "Kosef", "Malark", "Morn", "Randal", "Stedd"];
     let nomeHumanoF = ["Arveene", "Esvele", "Jhessail", "Katernin", "Kerri", "Lureene", "Miri", "Mara", "Natali", "Rowan", "Shandri", "Tessele"];
     let sobrenomeHumano = ["Amblecrown", "Bersk", "Buckman", "Chernin", "Chernin","Dundragon", "Evenwood", "Greycastle", "Kulenov", "Tallstag"];
-
-    let nomeMeioElfoM = ['Aelar', 'Bastian', 'Caelen', 'Dorian', 'Erevan', 'Faelar', 'Galen', 'Haelen', 'Ilian', 'Jareth', 'Kaelan', 'Lorian', 'Maelis'];
-    let nomeMeioElfoF = ['Elara', 'Aria', 'Lirael', 'Faela', 'Thalia', 'Elysia', 'Mira', 'Lyra', 'Seraphina', 'Evelina', 'Althea', 'Nerissa', 'Elowen'];
+    // Meio Elfo
+    let nomeMeioElfoM = ["Aelar", "Bastian", "Caelen", "Dorian", "Erevan", "Faelar", "Galen", "Haelen", "Ilian", "Jareth", "Kaelan", "Lorian", "Maelis"];
+    let nomeMeioElfoF = ["Elara", "Aria", "Lirael", "Faela", "Thalia", "Elysia", "Mira", "Lyra", "Seraphina", "Evelina", "Althea", "Nerissa", "Elowen"];
     let sobrenomeMeioElfo = ["Moonshadow", "Silverbrook", "Duskrunner", "Starfrost", "Stormdancer", "Gloomweaver", "Sunsong", "Evebane"];
+    // Draconato
+    let nomeDraconatoM = ["Adrex", "Arjhan", "Azzakh", "Balasar", "Bharash", "Donaar", "Ghesh", "Heskan", "Kriv", "Medrash", "Nadarr"];
+    let nomeDraconatoF = ["Vrakroth", "Zethra", "Sylvara", "Narixia", "Vixxa", "Zahara", "Myrth", "Syndra", "Lyranna", "Zephyr", "Veridia"];
+    let sobrenomeDraconato = ["Stormscale", "Fireforge", "Frostbreath", "Ironclaw", "Shadowtail", "Thunderstrike", "Goldscales", "Silverwing"];
+    // Tiefling
+    let nomeTieflingM = ["Zephyrus", "Lucius", "Maelis", "Valerian", "Dante", "Sylus", "Asmodeus", "Raziel", "Cyrus", "Kazimir", "Xander", "Ignatius", "Malachi"];
+    let nomeTieflingF = ["Akta", "Calista", "Damara", "Eshe", "Fiora", "Gizella", "Helene", "Isra", "Jezabel", "Kallista", "Lilith", "Mirella", "Nemeia"];
+    let sobrenomeTiefling = ["Hellsbane", "Shadowhorn", "Duskwalker", "Blackthorn", "Infernoheart", "Grimmshadow", "Nightshade", "Voidspike"];
+    // Halfling
+    let nomeHalflingM = ["Alton", "Barley", "Cade", "Dannad", "Eldon", "Finnan", "Garret", "Hugo", "Igor", "Jasper", "Kip"];
+    let nomeHalflingF = ["Daisy", "Piper", "Willow", "Hazel", "Molly", "Cora", "Wren", "Fiona", "Poppy", "Lola", "Gemma"];
+    let sobrenomeHalfling = ["Greenbottle", "Underhill", "Proudbottom", "Goodbarrel", "Brushgather", "Hilltopple", "Smallburrow", "Burrfoot"];
+    // Gnomo
+    let nomeGnomoM = ["Alston", "Bilbron", "Cobb", "Dink", "Eldon", "Fonkin", "Gimble", "Haffen", "Juniper", "Kip", "Nebin"];
+    let nomeFemaleF = ["Alvina", "Briar", "Cora", "Daphne", "Elara", "Fiona", "Gwendolyn", "Hazel", "Iris", "Juniper", "Kelda"];
+    let sobrenomeGnomo = ["Glimmerdust", "Copperpot", "Mossbeard", "Fizzlebang", "Whistlewhisker", "Proudbuckle", "Merrybrook", "Windlebottom"];
+    // Meio Orc
+    let nomeMeioOrcM =["Grommash", "Gorak", "Throk", "Drak'Thar", "Mogor", "Hakkar", "Durotan", "Kargath", "Zul'jin", "Nazgrim", "Gul'dan"];
+    let nomeMeioOrcF = ["Gruusha", "Zorana", "Ragga", "Lokara", "Urzula", "Ghazra", "Kragga", "Drakara", "Hrashka", "Kulga", "Gorga"];
+    let sobrenomeMeioOrc = ["Grimjaw", "Bloodtusk", "Skullcrusher", "Ironhide", "Ravenshadow", "Stormfang", "Blackthorn", "Dreadblade"];
 
     let raceP = objconvRace[race];
     let paramP = gender == "male" ? `nome${raceP}M` : `nome${raceP}F`;
@@ -96,66 +167,6 @@ function genClass() {
     return className;
 }
 
-function rdmColor() {
-    options = 'ABCDEF0123456789';
-    max = options.length;
-    function draw(){
-        opt = max;
-        let v = Math.floor(opt * Math.random());
-        return v;
-    }
-    var result = '';
-    var opacity = '';
-    for(i=1; i<7; i++){
-        idx = draw();
-        result += options[idx];
-    }
-    for(a=1; a<3; a++){
-        ix = draw();
-        opacity += options[ix];
-    }
-    return "#"+result+opacity;
-}
-
-function* genRdn() {
-    function keys(size) {
-        // options = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        // options = 'abcdefghijklmnopqrstuvwxyz0123456789';
-        options = 'abcdef0123456789';
-        max = options.length;
-        function draw(){
-            opt = max;
-            let v = Math.floor(opt * Math.random());
-            return v;
-        }
-        var result = '';
-        for(i = 1; i < parseInt(size) + 1; i++){
-            idx = draw();
-            result += options[idx];
-        }
-        return result;
-    }
-
-    function keyChain(s1,s2,s3,s4,s5) {
-
-        str1 = keys(s1);
-        str2 = keys(s2);
-        str3 = keys(s3);
-        str4 = keys(s4);
-        str5 = keys(s5);
-
-        keyBlock = str1 + '-' + str2 + '-' + str3 + '-' + str4 + '-' + str5;
-
-        return keyBlock;
-    }
-
-    var index = 0;
-    while(true) {
-        return keyChain(8,4,4,4,12)
-        yield index++
-    }
-}
-
 function rollDice(qtd, size) {
     function roll(){
         let v = Math.ceil(size * Math.random());
@@ -164,24 +175,45 @@ function rollDice(qtd, size) {
     res = [];
     for(i=1;i<=qtd;i++){
         val = roll()
-        // console.log('dado '+i+' =', val);
         res.push(val)
     }
-    res.sort().shift();
+    return res;
+}
+
+function explodeDices(diceArray) {
+    val = '';
+    for(let i=0; i < diceArray.length; i++){
+        val += `dado: ${(Number(i)+1)} -> ${diceArray[i]}` + '<br>';
+    }
+    return val;
+}
+
+function rollDiceAttr(qtd, size) {
+    function roll(){
+        let v = Math.ceil(size * Math.random());
+        return v;
+    }
+    res = [];
+    for(i=1;i<=qtd;i++){
+        val = roll()
+        res.push(val)
+    }
+    res.sort((a,b) => a - b).shift();
     return res.reduce((a,b)=>a+b);
 }
 
 function generateAttr() {
     arrVals = [];
-    val1 = rollDice(4,6);
-    val2 = rollDice(4,6);
-    val3 = rollDice(4,6);
-    val4 = rollDice(4,6);
-    val5 = rollDice(4,6);
-    val6 = rollDice(4,6);
+    val1 = rollDiceAttr(4,6);
+    val2 = rollDiceAttr(4,6);
+    val3 = rollDiceAttr(4,6);
+    val4 = rollDiceAttr(4,6);
+    val5 = rollDiceAttr(4,6);
+    val6 = rollDiceAttr(4,6);
 
     arrVals.push(val1,val2,val3,val4,val5,val6);
-    return arrVals.sort();
+    // return arrVals.sort();
+    return arrVals.sort((a,b) => a - b)
 }
 
 function raceAdjust(raceN, values) {
